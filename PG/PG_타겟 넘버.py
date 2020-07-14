167f49e5220ef839c3259f2c6971f01180ba5ae9
soonbum <ssshhh0402@naver.com>
2020-07-14 16:15:59 +0900

    20200714

---
+++ PG/PG_타겟 넘버.py
@@ -0,0 +1,23 @@
+def solution(numbers, target):
+    answer = 0
+    def find(numbers, finds, target):
+        idx = 0
+        if len(numbers) == len(finds):
+            if sum(finds) == target:
+                return 1
+            else:
+                return 0
+        else:
+            target_1 = finds
+            target_1.append(numbers[len(finds)])
+            idx += find(numbers, target_1, target)
+            target_1.pop()
+            target_1.append(-(numbers[len(finds)]))
+            idx += find(numbers,target_1, target)
+            return idx
+    answer += find(numbers, [numbers[0]], target)
+    answer += find(numbers, [-numbers[0]], target)
+    return answer
+
+
+print(solution([1, 1, 1, 1, 1], 3))
