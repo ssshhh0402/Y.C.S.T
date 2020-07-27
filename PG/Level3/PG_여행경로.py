25b2dfce0e091c6817a286e9b55c9bc46b777cee
soonbum <ssshhh0402@naver.com>
2020-07-27 11:25:13 +0900

    20200727오전

--- PG/Level3/PG_여행경로.py
+++ PG/Level3/PG_여행경로.py
@@ -1,36 +1,24 @@
 def solution(tickets):
-    from collections import deque
     answer = []
     base = {}
-    lists = ["ICN"]
-    def find(base, lists):
-        if len(lists) == len(base.keys())+1:
-            return lists
-        now = lists[-1]
-        temp = []
-        for toGo in sorted(base[now],reverse=True):
-            if toGo not in lists:
-                lists.append(toGo)
-                imsi = find(base, lists)
-                if imsi:
-                    temp = imsi
-                lists.pop()
-        return temp
-
+    dq = ["ICN"]
     for ticket in tickets:
-        if ticket[0] not in base.keys():
+        if ticket[0] not in base:
             base[ticket[0]] = [ticket[1]]
         else:
             base[ticket[0]].append(ticket[1])
-    for toGo in sorted(base["ICN"],reverse=True):
-        lists.append(toGo)
-        temp = find(base, lists)
-        if temp:
-            answer = temp
-        lists.pop()
-
+    for item in base.keys():
+        base[item].sort(reverse=True)
+    while dq:
+        now = dq[-1]
+        if now not in base or len(base[now]) == 0:
+            answer.append(dq.pop())
+        else:
+            dq.append(base[now][-1])
+            base[now].pop()
+    answer.reverse()
     return answer
 
 
-# print(solution([["ICN", "JFK"], ["HND", "IAD"], ["JFK", "HND"]]))
+print(solution([["ICN", "JFK"], ["HND", "IAD"], ["JFK", "HND"]]))
 print(solution([["ICN", "SFO"], ["ICN", "ATL"], ["SFO", "ATL"], ["ATL", "ICN"], ["ATL", "SFO"]]))
