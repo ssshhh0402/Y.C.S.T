219181a67dc25f8b66cb8a74264c8df91c18c303
soonbum <ssshhh0402@naver.com>
2020-07-28 16:51:34 +0900

    20200728 오후

--- PG/Level3/PG_순위.py
+++ PG/Level3/PG_순위.py
@@ -1,18 +1,24 @@
-def solution(n,results):
+def solution(n, results):
     answer = 0
     win = [[] for _ in range(n+1)]
     lose = [[] for _ in range(n+1)]
     for result in results:
         win[result[0]].append(result[1])
         lose[result[1]].append(result[0])
-    for person in range(1,n+1):
-        for loser in lose[person]:
-            for winner in win[loser]:
-                if winner != person:
-                    win[person].append(winner)
+
+    for idx in range(1, n+1):
+        for winner in win[idx]:
+            for people in win[winner]:
+                if people not in win[idx]:
+                    win[idx].append(people)
+        for loser in lose[idx]:
+            for person in lose[loser]:
+                if person not in lose[idx]:
+                    lose[idx].append(person)
     for idx in range(1, n+1):
         if len(win[idx]) + len(lose[idx]) >= n-1:
             answer += 1
     return answer
+
 
 print(solution(5, [[4, 3], [4, 2], [3, 2], [1, 2], [2, 5]]))
