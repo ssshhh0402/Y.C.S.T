9f551d3aea8bf42b0c47b914d57b1835ba7e9c11
soonbum <ssshhh0402@naver.com>
2020-08-05 11:06:14 +0900

    20200805 오전

--- PG/Level3/PG_경주로 건설.py
+++ PG/Level3/PG_경주로 건설.py
@@ -4,15 +4,15 @@ def solution(boards):
     N = len(boards)
     visited = [[0 for _ in range(N)] for _ in range(N)]
     visited[0][0] = 1
-    dq = deque([(1, 0, 'D', 200), (0, 1, 'R', 200)])
+    dq = deque([(0, 0, 'S', 0)])
 
     def getCost(idi, di, icost):
-        if idi in ['R','L'] and di in ['U','D'] or idi in ['U','D'] and di in ['R','L']:
-            return icost + 500
+        if idi == 'S':
+            return icost + 100
+        if idi in ['R', 'L'] and di in ['U', 'D'] or idi in ['U', 'D'] and di in ['R', 'L']:
+            return icost + 600
         else:
             return icost + 100
-    for item in dq:
-        visited[item[0]][item[1]] = 200
     while dq:
         for _ in range(len(dq)):
             ix, iy, idi, icost = dq.popleft()
@@ -21,15 +21,15 @@ def solution(boards):
                     answer = icost
                 else:
                     answer = min(answer, icost)
-                continue
             else:
                 for dx, dy, di in [(0, 1, 'R'), (0, -1, 'L'), (1, 0, 'D'), (-1, 0, 'U')]:
                     x, y, z = ix+dx, iy+dy, getCost(idi, di, icost)
                     if 0 <= x < N and 0 <= y < N and not boards[x][y]:
-                        if not visited[x][y] or visited[x][y] > z:
+                        if not visited[x][y] or visited[x][y] >= z:
                             visited[x][y] = z
                             dq.append((x, y, di, z))
     return answer
 
 
-print(solution([[0, 0, 0], [0, 0, 0], [0, 0, 0]]))
+# print(solution([[0, 0, 0], [0, 0, 0], [0, 0, 0]]))
+print(solution([[0, 0, 1, 0], [0, 0, 0, 0], [0, 1, 0, 1], [1, 0, 0, 0]]))
