02c38b6f74db5848c4f71757ee15fdc10b0af820
soonbum <ssshhh0402@naver.com>
2020-08-12 11:42:50 +0900

    20200812 오전

--- PG/Level3/PG_블록 이동하기.py
+++ PG/Level3/PG_블록 이동하기.py
@@ -2,7 +2,7 @@ def solution(board):
     from collections import deque
     answer = 0
     N = len(board)
-    dq = deque([((0, 0), (0, 1), 0 )])
+    dq = deque([((0, 0), (0, 1), -1)])
     visited = []
     while dq:
         for _ in range(len(dq)):
@@ -12,21 +12,20 @@ def solution(board):
                     answer = time
                 else:
                     answer = min(answer, time)
-                continue
             else:
                 for dx, dy in [(0,1), (1,0), (0,-1), (0,-1)]:
                     new_left, new_right = (left[0] + dx, left[1] + dy), (right[0] + dx, right[1] + dy)
                     if 0 <= new_left[0] < N and 0 <= new_left[1] and 0 <= new_right[0] < N and 0 <= new_right[1] < N:
-                        if (left, right, new_left, new_right) not in visited and board[new_left[0]][new_left[1]] and board[new_right[0]][new_right[1]]:
+                        if (left, right, new_left, new_right) not in visited and not board[new_left[0]][new_left[1]] and not board[new_right[0]][new_right[1]]:
                             visited.append((left, right, new_left, new_right))
                             dq.append((new_left, new_right, time + 1))
-                if left[0] == right[0]:             # 위- 아래일직선
+                if left[1] == right[1]:             # 위- 아래일직선
                     # 오른쪽 기준 회전
                     flag_right = False
                     flag_left = False
                     for dx, dy in [(1, 1), (0, 1)]:
                         new_right = (left[0] + dx, left[1] + dy)
-                        if 0 <= new_right[0] < N and 0 <= new_right[1]:
+                        if 0 <= new_right[0] < N and 0 <= new_right[1] < N:
                             if board[new_right[0]][new_right[1]] == 1:
                                 flag_right = True
                                 break
@@ -44,12 +43,12 @@ def solution(board):
                             break
                     if not flag_right:
                         new_right = (left[0] + 1, left[1] + 1)
-                        if (left, right, right, new_right) not in visited:
+                        if (left, right, right, new_right) not in visited and 0 <= new_right[0] < N and 0 <= new_right[1] < N:
                             visited.append((left, right, right, new_right))
                             dq.append((right, new_right, time + 1))
                     if not flag_left:
                         new_left = (left[0] - 1, left[1] - 1)
-                        if (left, right, new_left, right) not in visited:
+                        if (left, right, new_left, right) not in visited and 0 <= new_left[0] < N and 0 <= new_left[1] < N :
                             visited.append((left, right, new_left, right))
                             dq.append((new_left, right, time+1))
                     # 왼쪽 기준 회전
@@ -75,15 +74,15 @@ def solution(board):
                             break
                     if not flag_left:
                         new_left = right[0] - 1, right[1] - 1
-                        if (left, right, new_left, right) not in visited:
+                        if (left, right, new_left, right) not in visited and 0 <= new_left[0] < N and 0 <= new_left[1] < N:
                             visited.append((left, right, new_left, right))
                             dq.append((new_left, right, time + 1))
                     if not flag_right:
                         new_right = right[0] -1, right[1] + 1
-                        if (left, right, left, new_right) not in visited:
+                        if (left, right, left, new_right) not in visited and 0 <= new_right[0] < N and 0 <= new_right[1] < N:
                             visited.append((left, right, new_left, right))
                             dq.append((new_left, right, time + 1))
-                elif left[1] == right[1]:   # 좌-우 일직선
+                elif left[0] == right[0]:   # 좌-우 일직선
                     #오른쪽 기준 회전
                     flag_left = False
                     flag_right = False
@@ -101,14 +100,17 @@ def solution(board):
                             if board[new_right[0]][new_right[1]] == 1:
                                 flag_left = True
                                 break
+                        else:
+                            flag_left = True
+                            break
                     if not flag_right:
                         new_left = left[0] + 1, left[1] + 1
-                        if (left, right, new_left, right) not in visited:
+                        if (left, right, new_left, right) not in visited and 0<= new_left[0] < N and 0 <= new_left[1] < N:
                             visited.append((left, right, new_left, right))
                             dq.append((new_left, right, time + 1))
                     if not flag_left:
                         new_left = left[0] + 1, left[1] + 1
-                        if (left, right, new_left, right) not in visited:
+                        if (left, right, new_left, right) not in visited and 0<= new_left[0] < N and 0 <= new_left[1] < N:
                             visited.append((left, right, new_left, right))
                             dq.append((new_left, right, time + 1))
                     #왼쪽 기준 회전
@@ -126,7 +128,7 @@ def solution(board):
                     for dx, dy in [(1, -1), (1, 0)]:
                         new_right = right[0] + dx, right[1] + dy
                         if 0 <= new_right[0] < N and 0 <= new_right[1] < N:
-                            if board[new_right[0]][new_right][1] == 1:
+                            if board[new_right[0]][new_right[1]] == 1:
                                 flag_right = True
                                 break
                         else:
@@ -134,15 +136,16 @@ def solution(board):
                             break
                     if not flag_left:
                         new_left = right[0] -1, right[1] - 1
-                        if (left, right, new_left, right) not in visited:
+                        if (left, right, new_left, right) not in visited and 0<= new_left[0] < N and 0 <= new_left[1] < N:
                             visited.append((left, right, new_left, right))
                             dq.append((new_left, right, time + 1))
                     if not flag_right:
                         new_right = right[0] + 1, right[1] - 1
-                        if (left, right, left, new_right) not in visited:
+                        if (left, right, left, new_right) not in visited and 0 <= new_right[0] < N and 0 <= new_right[1] < N:
                             visited.append((left, right, left, new_right))
                             dq.append((left, new_right, time + 1))
 
     return answer
 
-print(solution())
+
+print(solution([[0, 0, 0, 1, 1], [0, 0, 0, 1, 0], [0, 1, 0, 1, 1], [1, 1, 0, 0, 1], [0, 0, 0, 0, 0]]))
