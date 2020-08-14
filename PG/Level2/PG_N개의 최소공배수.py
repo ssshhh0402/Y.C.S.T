cc1dd1df4b214764cc4c22c735e704df4bee8eb4
soonbum <ssshhh0402@naver.com>
2020-08-14 16:09:10 +0900

    20200814 오후-2

--- PG/Level2/PG_N개의 최소공배수.py
+++ PG/Level2/PG_N개의 최소공배수.py
@@ -1,21 +1,12 @@
 def solution(arr):
-    answer = 0
+    import math
     arr.sort()
-    base = [[] for _ in range(len(arr))]
-    idx = 1
-    while True:
-        flag = True
-        for item in range(len(arr)):
-            base[item].append(arr[item] * idx)
-        key = base[0][-1]
-        for baseItem in base:
-            if key not in baseItem:
+    while len(arr) > 1:
+        num_a = arr.pop()
+        num_b = arr.pop()
+        gcd = math.gcd(num_a, num_b)
+        arr.append((num_a * num_b) // gcd)
+    return arr[0]
 
-                flag = False
-                break
-        if flag:
-            return key
-        else:
-            idx += 1
 
 print(solution([6,2,8,14]))
