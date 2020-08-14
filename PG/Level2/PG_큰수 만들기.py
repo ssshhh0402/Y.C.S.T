f80f19e362163273b493209a8911488a882346b2
soonbum <ssshhh0402@naver.com>
2020-08-14 11:32:12 +0900

    20200814 오전

--- PG/Level2/PG_큰수 만들기.py
+++ PG/Level2/PG_큰수 만들기.py
@@ -1,14 +1,19 @@
 def solution(number, k):
     answer = ''
+    comp = -1
+    N = len(number)
     base = list(map(int, list(number)))
-    n = len(number)
-    idx = 0
-    target = n - k
-    while len(answer) != target:
-        temp = max(base[idx:target+1])
-        idx = base.index(temp) + 1
+    target = N - k
+    while len(answer) == target:
+        temp = 0
+        n = len(answer)
+        for j in range(comp+1, k+n+1):
+            if temp < base[j]:
+                comp = j
+                temp = base[j]
         answer += str(temp)
     return answer
 
 
-print(solution("1924", 2))
+# print(solution("1924", 2))
+print(solution("1231234", 3))
