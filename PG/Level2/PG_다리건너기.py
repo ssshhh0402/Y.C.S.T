3dbb49fa967673babc464adc6cd64e5be59c594e
ssshhh0402 <ssshhh0402@naver.com>
2020-08-21 17:01:01 +0900

    20200821 오후

--- PG/Level2/PG_다리건너기.py
+++ PG/Level2/PG_다리건너기.py
@@ -1,28 +1,27 @@
 def solution(bridge_length,weight, truck_weight):
     from collections import deque
     answer = 0
-    truck_weight.sort()
-    now = deque([(truck_weight[-1], -1)])
-    now_weight = truck_weight[-1]
-    truck_weight.pop()
+    truck_weight.sort(reverse=True)
+    now = deque([])
+    now_weight = 0
     while truck_weight or now:
-        if truck_weight:
-            if now_weight + truck_weight[-1] <= weight:
-                now_weight += truck_weight[-1]
-                now.append((truck_weight[-1], -1))
-                truck_weight.pop()
         N = len(now)
         if N:
             for _ in range(N):
-                weight, time = now.popleft()
+                t_weight, time = now.popleft()
                 if time + 1 == bridge_length:
-                    now_weight -= weight
+                    now_weight -= t_weight
                 else:
-                    now.append((weight, time+1))
+                    now.append((t_weight, time + 1))
+        if truck_weight:
+            if now_weight + truck_weight[-1] <= weight:
+                now_weight += truck_weight[-1]
+                now.append((truck_weight[-1], 0))
+                truck_weight.pop()
         answer += 1
     return answer
 
 
-#print(solution(2, 10, [7, 4, 5, 6]))
+print(solution(2, 10, [7, 4, 5, 6]))
 #print(solution(100,100,[10]))
-print(solution(100,100,[10, 10, 10, 10, 10, 10, 10, 10, 10, 10]))
+#print(solution(100,100,[10, 10, 10, 10, 10, 10, 10, 10, 10, 10]))
