233e4984e261b5705d63a07352ca8540c968f1ae
ssshhh0402 <ssshhh0402@naver.com>
2020-08-25 11:45:55 +0900

    20200825 오전

---
+++ PG/Level2/PG_괄호변환.py
@@ -0,0 +1,53 @@
+def solution(p):
+    answer = ''
+    base = []
+    p = list(p)
+    if not p:
+        return answer
+
+    def div(p):
+        left = 0
+        idx = 0
+        for word in range(len(p)):
+            if p[word] == '(':
+                left += 1
+            else:
+                if left > 0:
+                    left -= 1
+                    if not left:
+                        idx = word
+                else:
+                    return p[0:idx], p[idx:]
+
+    def check(matters):
+        count = 0
+        for matter in matters:
+            if matter == '(':
+                count += 1
+            else:
+                count -= 1
+            if count < 0:
+                return False
+        if not count:
+            return True
+        else:
+            return False
+
+    while p:
+        u, v = div(p)
+        if check(u):
+            base.append(u)
+            p = v
+        else:
+            answer += '('
+            for item in range(len(base)):
+                if item == 0:
+                    n = len(base[item])
+                    answer += base[item][1:n-1]
+                else:
+                    answer += base[item]
+            answer += ')'
+    return answer
+
+
+print(solution('aa'));
