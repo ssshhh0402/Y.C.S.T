27c8ff5c51c50e390f084e5cef34b2bb7b3a2eba
ssshhh0402 <ssshhh0402@naver.com>
2020-08-28 11:52:27 +0900

    20200828 오전

--- PG/Level3/PG_보석쇼핑.py
+++ PG/Level3/PG_보석쇼핑.py
@@ -1,18 +1,16 @@
 def solution(gems):
     N = len(set(gems))
     answer = 0, len(gems)
-    s, e = 0, N
+    s, e = [0, 0]
     used = {}
-    for idx in range(s, e):
-        used[gems[idx]] = 1
-    while s >= 0 and e < len(gems):
+    while s < len(gems) and e < len(gems):
         if len(used.keys()) == N:
             if e - s < answer[1] - answer[0]:
-                answer = s, e
+                answer = [s, e]
             else:
-                used[gems[e]] -= 1
-                if not used[gems[e]]:
-                    del used[gems[e]]
+                used[gems[s]] -= 1
+                if not used[gems[s]]:
+                    del used[gems[s]]
                 s += 1
         else:
             gem = gems[e]
@@ -21,9 +19,13 @@ def solution(gems):
             else:
                 used[gem] += 1
             e += 1
-    return answer[0]+1, answer[1]
+    return [answer[0]+1, answer[1]]
 
 
-print(solution(["DIA", "RUBY", "RUBY", "DIA", "DIA", "EMERALD", "SAPPHIRE", "DIA"]))
-print(solution(["AA", "AB", "AC", "AA", "AC"]))
-print(solution(["XYZ", "XYZ", "XYZ"]))
+# print(solution(["DIA", "RUBY", "RUBY", "DIA", "DIA", "EMERALD", "SAPPHIRE", "DIA"]))
+# print(solution(["AA", "AB", "AC", "AA", "AC"]))
+# print(solution(["XYZ", "XYZ", "XYZ"]))
+# print(solution(["a","b","c","d"]))
+# print(solution(["a","a","a","a"]))
+# print(solution(["a"]))
+print(solution(["a","b","a","b","c"]))
---
+++ PG/Level3/PG_보석쇼핑2.py
@@ -0,0 +1,28 @@
+def solution(gems):
+    TYPE_NUM = len(set(gems))
+    GEM_NUM = len(gems)
+    cur_shop = {gems[0]: 1}
+    cand = []
+    l_idx, r_idx = 0, 0
+    DIST, RESULT = 0, 1
+    while l_idx < GEM_NUM and r_idx < GEM_NUM:
+        if len(cur_shop) < TYPE_NUM:
+            r_idx += 1
+            if r_idx == GEM_NUM:
+                break
+            cur_shop[gems[r_idx]] = cur_shop.get(gems[r_idx], 0) + 1
+        else:
+            cand.append((r_idx-l_idx, [l_idx+1, r_idx+1]))
+            cur_shop[gems[l_idx]] -= 1
+            if cur_shop[gems[l_idx]] == 0:
+                del cur_shop[gems[l_idx]]
+            l_idx += 1
+
+    cand = sorted(cand, key=lambda x: (x[DIST], x[RESULT]))
+
+    return cand[0][RESULT]
+
+# print(solution(["DIA", "RUBY", "RUBY", "DIA", "DIA", "EMERALD", "SAPPHIRE", "DIA"]))
+# print(solution(["AA", "AB", "AC", "AA", "AC"]))
+# print(solution(["XYZ", "XYZ", "XYZ"]))
+print(solution(['a','b','a','b','c']))
