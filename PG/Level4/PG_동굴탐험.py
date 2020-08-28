c7b2df7893030f5cc9bf2458404c1ba7ad0f1d81
ssshhh0402 <ssshhh0402@naver.com>
2020-08-28 17:56:40 +0900

    20200828 오후

--- PG/Level4/PG_동굴탐험.py
+++ PG/Level4/PG_동굴탐험.py
@@ -1,23 +1,57 @@
 def solution(n, path, order):
     from collections import deque
-    answer = True
-    base = [[] for _ in range(n)]   #n번째 인덱스에서 가는 아이들
+    global answer, visited
+    answer = False
+    base = [[] for _ in range(n)]
     visited = [[0 for _ in range(n)] for _ in range(n)]
     for start, end in path:
         base[start].append(end)
-
+        base[end].append(start)
     for start, end in order:
         visited[end][start] = 1
     dq = deque([0])
+    routes = []
+
+    def find(now, base, routes):
+        global visited, answer
+        if len(set(now)) == len(base):
+            answer = True
+
+        start = now[-1]
+        for toGo in base[start]:
+            if (start, toGo) not in routes:
+                if sum(visited[toGo]):
+                    if visited[toGo][start] == 1:
+                        visited[toGo][start] = 0
+                        dq.append(toGo)
+                        routes.append((start, toGo))
+                        find(dq, base,routes)
+                        dq.pop()
+                        routes.pop()
+                        visited[toGo][start] = 1
+                else:
+                    dq.append(toGo)
+                    routes.append((start, toGo))
+                    find(dq, base,routes)
+                    routes.pop()
+                    dq.pop()
+
     for next in base[0]:
         if sum(visited[next]):
             if visited[next][0] == 1:
                 visited[next][0] = 0
+                routes.append((0, next))
                 dq.append(next)
-                find(dq,base,visited)
+                find(dq, base, routes)
+                dq.pop()
+                routes.pop()
+                visited[next][0] = 1
         else:
             dq.append(next)
-            find(dq, base, visited)
-
+            routes.append((0,next))
+            find(dq, base,routes)
+            routes.pop()
+            dq.pop()
+    return answer
 
 print(solution(9,[[0,1],[0,3],[0,7],[8,1],[3,6],[1,2],[4,7],[7,5]],[[8, 5], [6, 7],[4,1]]))
