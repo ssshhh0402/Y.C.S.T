d3abf9b872e71ce72e34a0c11625d95c449fc979
ssshhh0402 <ssshhh0402@naver.com>
2020-09-03 17:45:23 +0900

    20200903 오전

--- PG/Level4/PG_가사검색.py
+++ PG/Level4/PG_가사검색.py
@@ -1,13 +1,25 @@
+def divide(word):
+
+    start = -1
+    end = 0
+    for chr in range(len(word)):
+        if word[chr] == '?':
+            if start < 0:
+                start = chr
+            else:
+                end = chr
+    return start, end
+
 def find(target, words):
     answer = 0
+    start, end = divide(target)
     for word in words:
         if len(word) == len(target):
-            flag = True
-            for idx in range(len(word)):
-                if target[idx] != word[idx] and target[idx] != '?':
-                    flag = False
-                    break
-            if flag:
+            a = word[:start]
+            b = target[:start]
+            c = word[end+1:]
+            d = target[end+1:]
+            if word[: start] == target[:start] and word[end+1:] == target[end+1:]:
                 answer += 1
     return answer
 
