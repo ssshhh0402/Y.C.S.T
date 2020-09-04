8cc4532354d99f861eaa29c98f0c9b14f3842fd3
ssshhh0402 <ssshhh0402@naver.com>
2020-09-04 15:20:11 +0900

    20200904 오후

--- PG/Level4/PG_가사검색.py
+++ PG/Level4/PG_가사검색.py
@@ -1,14 +1,20 @@
+import sys
+sys.setrecursionlimit(10 ** 8)
 def find(target, base):
     n = len(target)
+    answer = 0
     if n not in base:
         return 0
     else:
         now = base[n]
-    answer = 0
     for chr in target:
         if chr != '?':
-            answer = now[chr]['count']
-            now = now[chr]
+            if chr in now:
+                answer = now[chr]['count']
+                now = now[chr]
+            else:
+                answer = 0
+                break
         else:
             break
     return answer
@@ -18,43 +24,42 @@ def solution(words, queries):
     base = {}
     re_base = {}
     for word in words:
-        idx = 0
         if len(word) in base:
             now = base[len(word)]
         else:
             base[len(word)] = {}
             now = base[len(word)]
-        while idx != len(word):
-            chr = word[idx]
+        for chr in word:
             if chr not in now:
                 now[chr] = {'count': 1}
             else:
                 now[chr]['count'] += 1
             now = now[chr]
-            idx += 1
     for word in words:
-        idx = 0
         if len(word) in re_base:
             now = re_base[len(word)]
         else:
             re_base[len(word)] = {}
             now = re_base[len(word)]
         word = word[::-1]
-        while idx != len(word):
-            chr = word[idx]
+        for chr in word:
             if chr not in now:
                 now[chr] = {'count' : 1}
             else:
                 now[chr]['count'] += 1
             now = now[chr]
-            idx += 1
     for query in queries:
         n = len(query)
-        if n == query.count("?"): 
-            answer.append(base[n])
+        if n == query.count("?"):
+            temps = 0
+            if n in base:
+                for temp in base[n]:
+                    temps += base[n][temp]['count']
+            answer.append(temps)
         elif not query.startswith("?"):
             answer.append(find(query, base))
         else:
             answer.append(find(query[::-1], re_base))
     return answer
 print(solution(["frodo", "front", "frost", "frozen", "frame", "kakao"], ["fro??", "????o", "fr???", "fro???", "pro?"]))
+print(solution(["frodo", "front", "frost", "frozen", "frame", "kakao"], ["????k"]))
