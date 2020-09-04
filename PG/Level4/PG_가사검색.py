c8fcaa6cc2b7969fab89d55c1436210a7fcab6b7
ssshhh0402 <ssshhh0402@naver.com>
2020-09-04 11:26:56 +0900

    20200904 오전

--- PG/Level4/PG_가사검색.py
+++ PG/Level4/PG_가사검색.py
@@ -1,98 +1,60 @@
-# def divide(word):
-#
-#     start = -1
-#     end = 0
-#     for chr in range(len(word)):
-#         if word[chr] == '?':
-#             if start < 0:
-#                 start = chr
-#             else:
-#                 end = chr
-#     return start, end
-#
-# def find(target, words):
-#     answer = 0
-#     start, end = divide(target)
-#     for word in words:
-#         if len(word) == len(target):
-#             if word[: start] == target[:start] and word[end+1:] == target[end+1:]:
-#                 answer += 1
-#     return answer
-#
-# def solution(words, queries):
-#     answer = []
-#     for query in queries:
-#         n = find(query, words)
-#         answer.append(n)
-#     return answer
-class Node:
-    def __init__(self, w=""):
-        self.word = w
-        self.nList = {}
-        self.length = 0
-
-class Trie:
-    def __init__(self):
-        self.root = Node("")
-        self.keyList = {}
-
-    def create(self, words, reverse = False):
-        for word in words:
-            if reverse:
-                word = word[::-1]
-            if word not in self.keyList:
-                self.keyList[word] = len(word)
-                cur_node = self.root
-                for w in word:
-                    if w not in cur_node.nList:
-                        cur_node.nList[w] = Node(w)
-                    cur_node = cur_node.nList[w]
-                cur_node.length = len(word)
-
-    def search(self, word):
-        num = 0
-        cur_node = self.root
-        for w in word:
-            if w not in cur_node.nList:
-                if w == '?':
-                    stack = []
-                    l = len(word)
-                    nQ = word.count("?")
-                    if nQ == l:
-                        for i in self.keyList.values():
-                            num += 1
-                        break
-                    for node in cur_node.nList.values():
-                        stack.append((node, 1))
-                    while stack:
-                        cur_node, cur_depth = stack.pop()
-                        if cur_node.length == len(word) and cur_depth == nQ:
-                            num += 1
-                        if cur_depth < nQ:
-                            for node in cur_node.nList.values():
-                                stack.append((node, cur_depth+1))
-                    break
-                else:
-                    break
-            cur_node = cur_node.nList[w]
-        return num
+def find(target, base):
+    n = len(target)
+    if n not in base:
+        return 0
+    else:
+        now = base[n]
+    answer = 0
+    for chr in target:
+        if chr != '?':
+            answer = now[chr]['count']
+            now = now[chr]
+        else:
+            break
+    return answer
 
 def solution(words, queries):
     answer = []
     base = {}
-    trie = Trie()
-    trie.create(words, False)
-    re_trie = Trie()
-    re_trie.create(words, True)
-    for query in queries:
-        if query not in base:
-            if query.startswith("?"):
-                answer.append(re_trie.search(query[::-1]))
-            else:
-                answer.append(trie.search(query))
-            base[query] = answer[-1]
+    re_base = {}
+    for word in words:
+        idx = 0
+        if len(word) in base:
+            now = base[len(word)]
         else:
-            answer.append(base[query])
+            base[len(word)] = {}
+            now = base[len(word)]
+        while idx != len(word):
+            chr = word[idx]
+            if chr not in now:
+                now[chr] = {'count': 1}
+            else:
+                now[chr]['count'] += 1
+            now = now[chr]
+            idx += 1
+    for word in words:
+        idx = 0
+        if len(word) in re_base:
+            now = re_base[len(word)]
+        else:
+            re_base[len(word)] = {}
+            now = re_base[len(word)]
+        word = word[::-1]
+        while idx != len(word):
+            chr = word[idx]
+            if chr not in now:
+                now[chr] = {'count' : 1}
+            else:
+                now[chr]['count'] += 1
+            now = now[chr]
+            idx += 1
+    for query in queries:
+        n = len(query)
+        if n == query.count("?"): 
+            answer.append(base[n])
+        elif not query.startswith("?"):
+            answer.append(find(query, base))
+        else:
+            answer.append(find(query[::-1], re_base))
     return answer
-
-print(solution(["frodo", "front", "frost", "frozen", "frame", "kakao"], ["fro??", "????o", "fr???", "fro???", "pro?", "f????"]))
+print(solution(["frodo", "front", "frost", "frozen", "frame", "kakao"], ["fro??", "????o", "fr???", "fro???", "pro?"]))
