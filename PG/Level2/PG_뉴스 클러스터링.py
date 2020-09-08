b7ba4f043485698f0d2265867a0990247b7acddf
ssshhh0402 <ssshhh0402@naver.com>
2020-09-08 16:17:10 +0900

    20200908 오후

--- PG/Level2/PG_뉴스 클러스터링.py
+++ PG/Level2/PG_뉴스 클러스터링.py
@@ -7,24 +7,25 @@ def find(target):
 
 
 def solution(str1, str2):
-    base = []
     rywlqgkq = []
-    gkqwlqgkq = set([])
-    base.append(find(str1))
-    base.append(find(str2))
-    used = [0 for _ in range(len(base[1]))]
-    for item in base[0]:
-        for idx in range(len(base[1])):
-            if item == base[1][idx] and not used[idx]:
+    str1 = find(str1)
+    str2 = find(str2)
+    used = [0 for _ in range(len(str2))]
+    for item in str1:
+        for idx in range(len(str2)):
+            if item == str2[idx] and not used[idx]:
                 used[idx] = 1
                 rywlqgkq.append(item)
                 break
-    for items in base:
-        for item in items:
-            gkqwlqgkq.add(item)
-    answer = (len(rywlqgkq) / len(gkqwlqgkq))
+
+    N = len(rywlqgkq)
+    M = len(str1) + len(str2) - N
+    if not M:
+        answer = 1
+    else:
+        answer = N / M
     return int(answer * 65536)
 
-#print(solution("FRANCE", "FRENCH"))
-#print(solution("handshake", "shakehands"))
+print(solution("FRANCE", "FRENCH"))
+print(solution("handshake", "shake hands"))
 print(solution("aa1+aa2", "aaaa12"))
