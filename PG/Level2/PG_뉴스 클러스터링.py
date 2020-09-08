a150bbe59da182e070728d852fe14ce88f1cb24d
ssshhh0402 <ssshhh0402@naver.com>
2020-09-08 11:41:57 +0900

    20200908 오전

--- PG/Level2/PG_뉴스 클러스터링.py
+++ PG/Level2/PG_뉴스 클러스터링.py
@@ -4,21 +4,27 @@ def find(target):
         if target[idx].isalpha() and target[idx+1].isalpha():
             answer.append(target[idx:idx+2].lower())
     return answer
+
+
 def solution(str1, str2):
     base = []
     rywlqgkq = []
     gkqwlqgkq = set([])
     base.append(find(str1))
     base.append(find(str2))
+    used = [0 for _ in range(len(base[1]))]
     for item in base[0]:
-        if item in base[1]:
-            rywlqgkq.append(item)
+        for idx in range(len(base[1])):
+            if item == base[1][idx] and not used[idx]:
+                used[idx] = 1
+                rywlqgkq.append(item)
+                break
     for items in base:
         for item in items:
             gkqwlqgkq.add(item)
     answer = (len(rywlqgkq) / len(gkqwlqgkq))
     return int(answer * 65536)
 
-print(solution("FRANCE", "FRENCH"))
-print(solution("handshake","shakehands"))
+#print(solution("FRANCE", "FRENCH"))
+#print(solution("handshake", "shakehands"))
 print(solution("aa1+aa2", "aaaa12"))
