cf1f4d7d8a922faf94a51a840431604a38ea8e16
ssshhh0402 <ssshhh0402@naver.com>
2020-09-09 17:49:36 +0900

    20200909 오후

--- PG/Level2/PG_압축.py
+++ PG/Level2/PG_압축.py
@@ -1,18 +1,23 @@
 def solution(msg):
     answer = []
     base = [0,'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
-    for message in range(len(msg)):
+    message = 0
+    while message != len(msg):
         temps = msg[message]
         answer.append(base.index(temps))
         idx = message + 1
-        while True:
-            temps += msg[idx]
-            if temps not in base:
-                base.append(temps)
-                break
-            else:
-                answer.append(base.index(temps))
-                idx += 1
+        if idx == len(msg):
+            break
+        else:
+            while idx != len(msg):
+                temps += msg[idx]
+                if temps not in base:
+                    base.append(temps)
+                    message = idx
+                    break
+                else:
+                    answer.append(base.index(temps))
+                    idx += 1
     return answer
 
 
