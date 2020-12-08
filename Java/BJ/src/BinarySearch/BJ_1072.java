f0a6bdc0e171727cde8798082db7d275bd4be267
ssshhh0402 <ssshhh0402@naver.com>
2020-12-08 22:33:32 +0900

    20201208

---
+++ Java/BJ/src/BinarySearch/BJ_1072.java
@@ -0,0 +1,32 @@
+package BinarySearch;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BJ_1072 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] inputs = br.readLine().split(" ");
+        long X = Long.parseLong(inputs[0]);
+        long Y = Long.parseLong(inputs[1]);
+        long Z = (Y*100) / X;
+        if(Z >= 99){
+            System.out.println(-1);
+        }else{
+            long start = 1;
+            long end = X;
+            long mid = 0;
+            while(start <= end){
+                mid = (start + end) / 2;
+                long rat = ((Y+mid) * 100) / (X+mid);
+                if (rat > Z){
+                    end = mid - 1;
+                }else{
+                    start = mid + 1;
+                }
+            }
+            System.out.println(start);
+        }
+    }
+}
