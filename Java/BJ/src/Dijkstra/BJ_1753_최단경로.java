4050fad1d31c96a7980cf3373e000dfb90cffaef
ssshhh0402 <ssshhh0402@naver.com>
2020-12-11 00:52:54 +0900

    20201210

---
+++ Java/BJ/src/Dijkstra/BJ_1753_최단경로.java
@@ -0,0 +1,15 @@
+package Dijkstra;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BJ_1753_최단경로 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] inputs = br.readLine().split(" ");
+        int V= Integer.parseInt(inputs[0]);
+        int E = Integer.parseInt(inputs[1]);
+        int K = Integer.parseInt(br.readLine());
+    }
+}
