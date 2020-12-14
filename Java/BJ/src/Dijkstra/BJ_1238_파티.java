0061e6ba3932edfc9f2a86f23a1492b744c3d802
ssshhh0402 <ssshhh0402@naver.com>
2020-12-14 12:59:49 +0900

    20201214

--- Java/BJ/src/Dijkstra/BJ_1238_파티.java
+++ Java/BJ/src/Dijkstra/BJ_1238_파티.java
@@ -18,15 +18,41 @@ class P_Pair implements Comparable<P_Pair>{
     }
         }
 public class BJ_1238_파티 {
+    static ArrayList<P_Pair>[] base;
+    static PriorityQueue<P_Pair> pq;
+    public static int dijk(int start, int end, int N){
+        pq = new PriorityQueue<P_Pair>();
+        int[] dist = new int[N+1];
+        boolean [] visited = new boolean[N+1];
+        Arrays.fill(dist, Integer.MAX_VALUE);
+        dist[start] = 0;
+        pq.add(new P_Pair(start, 0));
+        while(!pq.isEmpty()){
+            P_Pair now = pq.poll();
+            if(visited[now.end]){
+                continue;
+            }
+            visited[now.end] = true;
+            for(P_Pair toGo : base[now.end]){
+                int next = toGo.end;
+                int weight = toGo.weight;
+                if(dist[next] > dist[now.end] + weight){
+                    dist[next] = dist[now.end] + weight;
+                    pq.add(new P_Pair(next, dist[next]));
+                }
+            }
+        }
+        return dist[end];
+    }
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String[] inputs = br.readLine().split(" ");
         int N = Integer.parseInt(inputs[0]);
         int M = Integer.parseInt(inputs[1]);
         int X = Integer.parseInt(inputs[2]);
-        int answer = Integer.MAX_VALUE;
-        ArrayList<P_Pair>[] base = new ArrayList[N+1];
-        PriorityQueue<P_Pair> pq;
+        int [] temps = new int [N+1];
+        int answer = Integer.MIN_VALUE;
+        base = new ArrayList[N+1];
         for(int idx = 0; idx < N+1; idx++){
             base[idx] = new ArrayList<P_Pair>();
         }
@@ -38,30 +64,11 @@ public class BJ_1238_파티 {
             base[start].add(new P_Pair(end, weight));
         }
         for(int idx = 1; idx < N+1; idx++){
-            pq = new PriorityQueue<P_Pair>();
-            int[] dist = new int[N+1];
-            boolean [] visited = new boolean[N+1];
-            Arrays.fill(dist, Integer.MAX_VALUE);
-            dist[idx] = 0;
-            pq.add(new P_Pair(idx, 0));
-            while(!pq.isEmpty()){
-                P_Pair now = pq.poll();
-                if(visited[now.end]){
-                    continue;
-                }
-                visited[now.end] = true;
-                for(P_Pair toGo : base[now.end]){
-                    int next = toGo.end;
-                    int n_weight = toGo.weight;
-                    if(dist[next] > dist[now.end] + n_weight){
-                        dist[next] = dist[now.end] + n_weight;
-                        pq.add(new P_Pair(next, dist[next]));
-                    }
-                }
-            }
-            if
-
+            temps[idx] = dijk(idx,X,N) + dijk(X,idx,N);
         }
-
+        for(int item : temps) {
+            answer = Math.max(answer, item);
+        }
+        System.out.println(answer);
     }
 }
