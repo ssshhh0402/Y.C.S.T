36f8ced1d29d6593c518d79836e6eb0e3e7b3d61
ssshhh0402 <ssshhh0402@naver.com>
2020-12-21 22:56:06 +0900

    20201221

--- Java/PG/src/징검다리건너기.java
+++ Java/PG/src/징검다리건너기.java
@@ -1,28 +1,28 @@
 public class 징검다리건너기 {
-    public static int cnt(int[] stones, long mid){
-        int count = 0;
-        for(int stone : stones){
-            if(stone - mid <= 0){
-                count += 1;
-            }else{
-                count = 0;
-            }
-            if(count > mid){
-                return count;
-            }
-        }
-        return count;
-    }
     public static int solution(int[] stones, int k){
         int answer = 0;
         long start = 0;
         long end = Integer.MAX_VALUE;
         long mid;
+        int count;
+        boolean flag;
         while (start <= end){
             mid = (start + end) / 2;
-            int count = cnt(stones, mid);
-            if(count < k){
-                answer = ((int)mid);
+            count = 0;
+            flag = true;
+            for(int stone : stones){
+                if(stone - (mid-1) <= 0){
+                    count += 1;
+                }else{
+                    count = 0;
+                }
+                if( count >= k){
+                    flag = false;
+                    break;
+                }
+            }
+            if (flag){
+                answer = (int)mid;
                 start = mid + 1;
             }else{
                 end = mid - 1;
@@ -32,5 +32,6 @@ public class 징검다리건너기 {
     }
     public static void main(String[] args) {
         System.out.println(solution(new int [] {2, 4, 5, 3, 2, 1, 4, 2, 5, 1},3));
+        System.out.println(Integer.MAX_VALUE - 200000001);
     }
 }
