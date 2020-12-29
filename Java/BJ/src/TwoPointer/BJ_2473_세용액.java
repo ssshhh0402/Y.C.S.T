3da765a36956f4547f07301cad14f394d98e3d6d
ssshhh0402 <ssshhh0402@naver.com>
2020-12-29 17:45:37 +0900

    20201229

--- Java/BJ/src/TwoPointer/BJ_2473_세용액.java
+++ Java/BJ/src/TwoPointer/BJ_2473_세용액.java
@@ -9,12 +9,39 @@ public class BJ_2473_세용액 {
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
-        int[] base = new int[N];
+        long [] base = new long[N];
         String[] inputs = br.readLine().split(" ");
         for(int i = 0 ; i < N; i++){
-            base[i] = Integer.parseInt(inputs[i]);
+            base[i] = Long.parseLong(inputs[i]);
         }
         Arrays.sort(base);
-        int s=0,e=N-1,
+        int idx=0;
+        long[] answers = new long[3];
+        long answer = 3000000000L;
+        while(idx != N-2){
+            long temps = base[idx];
+            int s=idx+1,e=N-1;
+            while(s < e && e < N){
+                long now = base[s] + base[e] + temps;
+                if(Math.abs(now) < answer){
+                    answer = Math.abs(now);
+                    answers[0] = base[s];
+                    answers[1] = base[idx];
+                    answers[2] = base[e];
+                }
+                if(now < 0){
+                    s += 1;
+                }else{
+                    e -= 1;
+                }
+            }
+            idx += 1;
+        }
+        Arrays.sort(answers);
+        String a = "";
+        for(int i = 0 ; i < 3; i++){
+            a += String.valueOf(answers[i]) + " ";
+        }
+        System.out.println(a);
     }
 }
