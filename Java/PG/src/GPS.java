e095a7779ace7de4adc17d09932eee9be6da7312
ssshhh0402 <ssshhh0402@naver.com>
2021-01-02 00:33:08 +0900

    20210101

--- Java/PG/src/GPS.java
+++ Java/PG/src/GPS.java
@@ -3,7 +3,6 @@ import java.util.Arrays;
 
 public class GPS {
     public static int solution(int n, int m, int [][] edge_list, int k, int [] gps_log){
-        int answer = 0;
         ArrayList<Integer>[] base = new ArrayList[n+1];
         for(int i = 0 ; i < n+1; i++){
             base[i] = new ArrayList<Integer>();
@@ -19,7 +18,25 @@ public class GPS {
             Arrays.fill(dp[i], Integer.MAX_VALUE);
         }
         dp[0][gps_log[0]] = 0;
-        return answer;
+        for(int i = 1; i < k; i++){
+            for(int j = 1; j < n+1; j++){
+                dp[i][j] = Math.min(dp[i][j], dp[i-1][j]);
+                for(int toGo : base[j]){
+                    dp[i][j] = Math.min(dp[i-1][toGo], dp[i][j]);
+                }
+                if(gps_log[i] != j){
+                    if(dp[i][j] != Integer.MAX_VALUE){
+                        dp[i][j] += 1;
+                    }
+                }
+            }
+        }
+        int answer = dp[k-1][gps_log[k-1]];
+        if(answer == Integer.MAX_VALUE){
+            return -1;
+        }else{
+            return answer;
+        }
     }
     public static void main(String[] args){
         System.out.println(solution());
