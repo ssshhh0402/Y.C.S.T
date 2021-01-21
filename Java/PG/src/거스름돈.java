5c80170b4684e5376c0b6dee47056f33666924d7
ssshhh0402 <ssshhh0402@naver.com>
2021-01-21 16:16:50 +0900

    20210121

---
+++ Java/PG/src/거스름돈.java
@@ -0,0 +1,14 @@
+import java.util.Arrays;
+
+public class 거스름돈 {
+    public static int solution(int n, int[] money){
+        int [][] dp = new int[money.length+1][n+1];
+        Arrays.sort(money);
+        for(int i = 1; i < n+1; i++){
+            if()
+        }
+    }
+    public static void main(String[] args){
+        System.out.println(solution(5,new int [] {1,2,5} ));
+    }
+}
