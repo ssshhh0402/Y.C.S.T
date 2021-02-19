9d4192ee2c07cc4006eb7a4e4605b361da3d421b
ssshhh0402 <ssshhh0402@naver.com>
2021-02-19 17:51:41 +0900

    20210219

--- Java/BJ/src/DP/BJ_11727_2타일링2.java
+++ Java/BJ/src/DP/BJ_11727_2타일링2.java
@@ -4,19 +4,20 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class BJ_1904_01타일 {
+public class BJ_11727_2타일링2 {
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
         int [] dp = new int[N+1];
-
         for(int i = 1; i < N+1; i++){
-            if(i < 3){
-                dp[i] = i % 15746;
+            if(i == 1){
+                dp[i] = 1;
+            }else if(i == 2){
+                dp[i] = 3;
             }else {
-                dp[i] = (dp[i - 1] + dp[i - 2]) % 15746;
+                dp[i] = (2 * dp[i - 2] + dp[i - 1]) % 10007;
             }
         }
-        System.out.println(dp[N] % 15746);
+        System.out.println(dp[N] % 10007);
     }
 }
