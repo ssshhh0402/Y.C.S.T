7275bef6afac96844e2f8c830d43883b59bf92d9
ssshhh0402 <ssshhh0402@naver.com>
2021-02-19 18:01:15 +0900

    20210219

---
+++ Java/BJ/src/DP/BJ_11726_2타일링.java
@@ -0,0 +1,23 @@
+package DP;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BJ_11726_2타일링 {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int[] dp = new int[N+1];
+        for(int i = 1; i < N+1; i++){
+            if(i == 1){
+                dp[i] = 1;
+            }else if(i == 2){
+                dp[i] = 2;
+            }else{
+                dp[i] = (dp[i-1] + dp[i-2]) % 10007;
+            }
+        }
+        System.out.println(dp[N] % 10007);
+    }
+}
