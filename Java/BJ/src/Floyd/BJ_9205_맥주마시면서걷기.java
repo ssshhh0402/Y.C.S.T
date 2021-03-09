094e3e8b759bc032bb207935a8a3a856d83a0d09
ssshhh0402 <ssshhh0402@naver.com>
2021-03-09 20:58:17 +0900

    20210309

---
+++ Java/BJ/src/Floyd/BJ_9205_맥주마시면서걷기.java
@@ -0,0 +1,15 @@
+package Floyd;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BJ_9205_맥주마시면서걷기 {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine());
+        while(T -- > 0){
+            System.out.println(T);
+        }
+    }
+}
