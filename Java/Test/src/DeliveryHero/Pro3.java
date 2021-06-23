27c5c90c1e3e5c0a661729fdfe861e66f97b900d
ssshhh0402 <ssshhh0402@naver.com>
2021-06-23 18:10:43 +0900

    20210623

--- Java/Test/src/DeliveryHero/Pro3.java
+++ Java/Test/src/DeliveryHero/Pro3.java
@@ -5,25 +5,50 @@ import java.io.IOException;
 public class Pro3 {
     public static String solution(int U, int L, int [] C) {
         int N = C.length;
+        boolean flag = true;
         int[][] base = new int[2][N];
-        int[][] dp_row = new int[2][N];
-        int[][] dp_col = new int[2][N];        //호율성을 따지는 걸로 보아 dp문제 인거같은데..
-        //식이 생각안난다...
-        for (int i = 0; i < N; i++) {
-            if (C[i] == 2) {
-                if (i == 0) {
-                    dp_row[0][i] = 1;
-                    dp_row[1][i] = 1;
-                    dp_col[0][i] = 1;
-                    dp_col[1][i] = 1;
-                } else {
-                    int min = Math.min(dp_row[0][i - 1], dp_row[1][i - 1]);
-                    dp_row[0][i] = min;
-                    dp_row[1][i] = min + 1;
+        int [] temps = new int [] {U, L};
+        for(int x = 0; x < 2; x++){
+            if(!flag){
+                break;
+            }
+            for(int y = 0; y < N; y++){
+                if(C[y] == 2){
+                    if(temps[x] != 0){
+                        C[y]-=1;
+                        base[x][y] = 1;
+                        temps[x] -= 1;
+                    }else{
+                        flag = false;
+                        break;
+                    }
+                }else if(C[y] == 1){
+                    if(temps[x] != 0){
+                        C[y]-=1;
+                        base[x][y] = 1;
+                        temps[x] -= 1;
+                    }
                 }
             }
         }
-        return "IMPPOSIBLE";
+        for(int temp : temps){
+            if(temp != 0){
+                flag = false;
+                break;
+            }
+        }
+        if(!flag){
+            return "IMPPOSIBLE";
+        }else{
+            StringBuilder sb = new StringBuilder();
+            for(int x = 0 ; x < 2; x++){
+                for(int y = 0; y < N; y++){
+                    sb.append(base[x][y]).append(" ");
+                }
+                sb.append("\n");
+            }
+            return (sb.toString());
+        }
     }
     public static void main(String[] args) throws IOException {
         System.out.println(solution(3, 2, new int [] {2, 1, 1, 0, 1}));
