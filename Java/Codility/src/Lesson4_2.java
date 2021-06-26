feb1e199d439edf3cc2b7d2d897007e159e8b2fc
ssshhh0402 <ssshhh0402@naver.com>
2021-06-26 17:19:04 +0900

    20210626

--- Java/Codility/src/Lesson4_2.java
+++ Java/Codility/src/Lesson4_2.java
@@ -1,18 +1,19 @@
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.PriorityQueue;
 
 public class Lesson4_2 {
     public static int [] solution(int N, int [] A){
         int[] answers = new int[N];
+        int max = 0, temps = 0;
         for(int a : A){
-            if(a  == (N+1)){
-                int [] temps = Arrays.copyOf(answers, N);
-                Arrays.sort(temps);
-                Arrays.fill(answers, temps[N-1]);
+            if (a == N+1){
+                max = temps;
             }else{
-                answers[a-1] += 1;
+                answers[a-1] = Math.max(answers[a-1]+1, max + 1);
+                temps = Math.max(answers[a-1],temps);
+            }
+        }
+        for(int i = 0 ; i < N; i++){
+            if(answers[i] < max){
+                answers[i] = max;
             }
         }
         return answers;
