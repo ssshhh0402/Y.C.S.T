bacd45bcb5a93061673ca3f0108b7c728414bdd2
ssshhh0402 <ssshhh0402@naver.com>
2021-07-26 13:41:20 +0900

    20210726

--- Java/PG/src/두개이하비트.java
+++ Java/PG/src/두개이하비트.java
@@ -1,4 +1,12 @@
 public class 두개이하비트 {
+    public static int [] solution(int [] numbers){
+        int [] answers = new int[numbers.length];
+        for(int number : numbers){
+            String target = Integer.toBinaryString(number);
+            System.out.println(target);
+        }
+        return answers;
+    }
     public static void main(String[] args){
         int [] numbers = new int [] {2,7};
         int [] answers = solution(numbers);
