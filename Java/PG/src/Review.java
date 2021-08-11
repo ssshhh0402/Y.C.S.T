4e238c65cdec016e11486591e5b6aa88747fff9d
ssshhh0402 <ssshhh0402@naver.com>
2021-08-11 18:10:56 +0900

    20210811|Review3

--- Java/PG/src/Review.java
+++ Java/PG/src/Review.java
@@ -1,34 +1,49 @@
-import java.util.LinkedList;
-import java.util.Queue;
 
 public class Review {
-    public static int solution(int n, int [][] computers){
-        int answer = 0;
-        boolean [] used = new boolean[n];
-        Queue<Integer> q = new LinkedList<Integer>();
-        for(int i = 0; i < n; i++){
-            if(!used[i]){
-                answer += 1;
-                q.add(i);
-                while(!q.isEmpty()){
-                    int x = q.poll();
-                    for(int y = 0; y < n; y++){
-                        if(computers[x][y] == 1 && !used[y]){
-                            used[y] = true;
-                            q.add(y);
-                        }
-                    }
+    static int answer,n;
+    static boolean[] used;
+    public static boolean isPossible(String one, String two){
+        int count = 0;
+        for(int i = 0; i < one.length(); i++){
+            if(one.charAt(i) != two.charAt(i)){
+                count += 1;
+                if(count >= 2){
+                    return false;
                 }
             }
         }
-        return answer;
+        return true;
+    }
+    public static void find(String now, String target, String[] words, int count){
+        if (now.equals(target)){
+            answer = Math.min(answer, count);
+            return;
+        }
+        for(int i = 0; i < words.length; i++){
+            if(!used[i] && isPossible(now, words[i])){
+                used[i] = true;
+                find(words[i], target, words, count+1);
+                used[i] = false;
+            }
+        }
+    }
+    public static int solution(String begin, String target, String [] words){
+        answer = Integer.MAX_VALUE;
+        n = words.length;
+        used = new boolean[n];
+        find(begin, target, words,0);
+        if(answer == Integer.MAX_VALUE){
+            return 0;
+        }else{
+            return answer;
+        }
     }
     public static void main(String[] args){
-        int n = 3;
-        int [][] computers = new int [][] {{1,1,0}, {1,1,0},{0,0,1}};
-        System.out.println(solution(n, computers));
-        n = 3;
-        computers = new int [][] {{1,1,0},{1,1,1},{0,1,1}};
-        System.out.println(solution(n,computers));
+        String begin = "hit";
+        String target = "cog";
+        String [] words = new String [] {"hot","dot","dog","lot","log","cog"};
+        System.out.println(solution(begin, target, words));
+        words = new String [] {"hot","dot","dog","lot","log"};
+        System.out.println(solution(begin, target, words));
     }
 }
