c4dda60dbfe48da0b6f4ecf20ea09f110d5ba88e
ssshhh0402 <ssshhh0402@naver.com>
2021-08-20 01:23:23 +0900

    20210820|Review

--- Java/PG/src/브라이언의고민.java
+++ Java/PG/src/브라이언의고민.java
@@ -3,42 +3,64 @@ import java.util.Queue;
 
 public class 브라이언의고민 {
     public static String solution(String sentence) {
-        StringBuilder sb = new StringBuilder();
-        Queue<Character> q = new LinkedList<Character>();
+        StringBuilder answer = new StringBuilder();
+        StringBuilder temps = new StringBuilder();
+        int count = 0;
+        int flag = 0;
         for(int i = 0 ; i < sentence.length(); i++){
             char now = sentence.charAt(i);
             if(Character.isUpperCase(now)){
-                q.add(now);
+                count += 1;
+                temps.append(now);
             }else{
-                if(sentence.charAt(i+2) == now){
+                if((i+2) < sentence.length() && sentence.charAt(i+2) == now){
                     int j = i+2;
                     while(j < sentence.length()){
-                        q.add(sentence.charAt(j-1));
-                        if(sentence.charAt(j+2) == now){
+                        temps.append(sentence.charAt(j-1));
+                        count += 1;
+                        if((j+2) < sentence.length() && sentence.charAt(j+2) == now){
                             j += 2;
                         }else{
-                            i = j;
+                            temps.append(sentence.charAt(j+1));
+                            answer.append(temps.toString()).append(" ");
+                            flag += 1;
+                            temps = new StringBuilder();
+                            i = j+1;
+                            break;
                         }
                     }
                 }else{
-                    for(int j = i; j < sentence.length(); j++){
+                    for(int j = i+1; j < sentence.length(); j++){
                         char target = sentence.charAt(j);
                         if(Character.isLowerCase(target)){
                             if(target == now){
+                                answer.append(temps.toString()).append(" ");
+                                flag += 1;
+                                temps = new StringBuilder();
                                 i = j;
                                 break;
                             }else{
                                 return "invalid";
                             }
+                        }else{
+                            temps.append(target);
+                            count += 1;
                         }
                     }
                 }
             }
         }
-        return sb.toString();
+        int kkk = sentence.length();
+        flag -= 1;
+        answer.deleteCharAt(answer.length()-1);
+        if(answer.length() + count - flag == sentence.length()){
+            return answer.toString();
+        }else{
+            return "invalid";
+        }
     }
     public static void main(String[] args){
-//        System.out.println(solution("HaEaLaLaObWORLDb"));
+        System.out.println(solution("HaEaLaLaObWORLDb"));
         System.out.println(solution("SpIpGpOpNpGJqOqA"));
         System.out.println(solution("AxAxAxAoBoBoB"));
     }
