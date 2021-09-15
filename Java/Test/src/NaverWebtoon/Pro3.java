d432f944bff234638be23202a1a06010b1c25276
ssshhh0402 <ssshhh0402@naver.com>
2021-09-16 01:08:49 +0900

    20210916

--- Java/Test/src/NaverWebtoon/Pro3.java
+++ Java/Test/src/NaverWebtoon/Pro3.java
@@ -7,7 +7,7 @@ public class Pro3 {
     public static void print(int a, int b, int count, int len){
         int N = len / options[count];
         if(count == 0){
-            for(int y = b; y < a+N+1; y++){
+            for(int y = b; y < b+N+1; y++){
                 answers[a][y] = value;
                 value += 1;
             }
@@ -19,16 +19,29 @@ public class Pro3 {
                 answers[a+len-1][y] = value;
                 value += 1;
             }
-            for(int x = a+len-1; x > a; x--){
+            for(int x = a+len-2; x > a; x--){
                 answers[x][b] = value;
                 value += 1;
             }
 
         }else{
-            for(int x=  a; x < a+len; x+=N){
-                for(int y = b; y < b+len; y += N){
-                    print(x, y, count-1, N);
-                }
+//            for(int x = a; x < a+len; x+=N){
+//                for(int y = b; y < b+len; y += N){
+//                    print(x, y, count-1, N);
+//
+//                }
+//            }
+            for(int y = a; y < b+len; y+=N){
+                print(a, y, count-1, N);
+            }
+            for(int x = b+N; x < a+len; x+=N){
+                print(x,b+N-1, count-1, N);
+            }
+            for(int y = b+len-(2*N); y >= b; y-=N){
+                print(a+N-1, y, count-1, N);
+            }
+            for(int x = a+len-(2*N); x > a; x+=N){
+                print(x, b, count-1, N);
             }
         }
     }
