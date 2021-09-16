e06def596484ecdd30901f4c4b9ade3c06f1444a
ssshhh0402 <ssshhh0402@naver.com>
2021-09-16 18:51:35 +0900

    20210916

--- Java/Test/src/NaverWebtoon/Pro3.java
+++ Java/Test/src/NaverWebtoon/Pro3.java
@@ -7,41 +7,39 @@ public class Pro3 {
     public static void print(int a, int b, int count, int len){
         int N = len / options[count];
         if(count == 0){
-            for(int y = b; y < b+N+1; y++){
-                answers[a][y] = value;
-                value += 1;
+            int target = value + len * len;
+            int flag = 0;
+            while(value < target){
+                for(int y = b+flag; y < b+len-flag; y++){
+                    answers[a+flag][y] = value++;
+                }
+                for(int x = a+flag+1; x < a+len-flag; x++){
+                    answers[x][b+len-flag-1] = value++;
+                }
+                for(int y = b+len-flag-2; y > b+flag-1; y--) {
+                    answers[a + len - flag-1][y] = value++;
+                }
+                for(int x = a + len - flag-2; x > a+flag; x--){
+                    answers[x][b+flag] = value++;
+                }
+                flag += 1;
             }
-            for(int x = a+1; x < a+len; x++){
-                answers[x][b+len-1] = value;
-                value += 1;
-            }
-            for(int y = b+len-2; y >= b; y--){
-                answers[a+len-1][y] = value;
-                value += 1;
-            }
-            for(int x = a+len-2; x > a; x--){
-                answers[x][b] = value;
-                value += 1;
-            }
-
         }else{
-//            for(int x = a; x < a+len; x+=N){
-//                for(int y = b; y < b+len; y += N){
-//                    print(x, y, count-1, N);
-//
-//                }
-//            }
-            for(int y = a; y < b+len; y+=N){
-                print(a, y, count-1, N);
-            }
-            for(int x = b+N; x < a+len; x+=N){
-                print(x,b+N-1, count-1, N);
-            }
-            for(int y = b+len-(2*N); y >= b; y-=N){
-                print(a+N-1, y, count-1, N);
-            }
-            for(int x = a+len-(2*N); x > a; x+=N){
-                print(x, b, count-1, N);
+            // 위, 오른, 아래 반복
+            int target = value + (len * len);
+            int flag = 0;
+            while(value < target){
+                System.out.print("NOW :" + flag);
+                for(int y = b+flag; y < b+len; y +=N){
+                    print(a, y, count-1, N);
+                }
+                for(int x = a + N - flag; x < a + len; x += N){
+                    print(x, b+N-flag, count-1, N);
+                }
+                for(int y = b+len-(2*N) - flag; y >= b; y-=N){
+                    print(a+N-flag, y, count-1, N);
+                }
+                flag += 1;
             }
         }
     }
@@ -58,10 +56,27 @@ public class Pro3 {
     }
     public static void main(String[] args){
         int [] length = new int [] {2,2};
-        int [][] answers = solution(length);
-        for(int x = 0 ; x < answers.length; x++){
-            for(int y = 0; y < answers[x].length; y++){
-                System.out.print(answers[x][y] + " ") ;
+//        int [][] answers = solution(length);
+//        for(int x = 0 ; x < answers.length; x++){
+//            for(int y = 0; y < answers[x].length; y++){
+//                System.out.print(answers[x][y] + " ") ;
+//            }
+//            System.out.println("");
+//        }
+//        length = new int [] {3,2};
+//        int [][] answers = solution(length);
+//        for(int x = 0 ; x < answers.length; x++){
+//            for(int y = 0; y < answers[x].length; y++){
+//                System.out.print(answers[x][y] + " ");
+//            }
+//            System.out.println("");
+//        }
+        System.out.println("-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_");
+        length = new int [] {4,3,2};
+        answers = solution(length);
+        for(int x = 0 ; x  <answers.length; x++){
+            for(int y = 0 ; y < answers[x].length; y++){
+                System.out.print(answers[x][y] + " ");
             }
             System.out.println("");
         }
