f691951767871e61c290a261d0ec4e27ec6457ed
ssshhh0402 <ssshhh0402@naver.com>
2021-10-05 17:37:48 +0900

    20211005

---
+++ Java/PG/src/LV3/리틀프렌즈사천성.java
@@ -0,0 +1,77 @@
+package LV3;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class 리틀프렌즈사천성 {
+    static boolean [][] visited;
+    static String [][] boards;
+    static ArrayList<String> answers;
+    static int [] dx = new int [] {0,1,0,-1};
+    static int [] dy = new int [] {1,0,-1,0};
+    static class Item{
+        int x, y;
+        boolean flag;
+
+        Item(int a, int b){
+            this.x = a;
+            this.y = b;
+            this.flag = false;
+        }
+    }
+    public static boolean isPossible(int x, int y){
+        Queue<Item> q = new LinkedList<Item>();
+
+        q.add(new Item(x,y));
+        String target = boards[x][y];
+    }
+    public static void find(int count, String route){
+        if(count == 0){
+            answers.add(route);
+            return;
+        }
+
+    }
+    public static String solution(int m, int n, String[] board){
+        visited = new boolean[m][n];
+        boards = new String[m][n];
+        int count = 0;
+        for(int x = 0; x < m; x++){
+            String [] now = board[x].split("");
+            for(int y = 0; y < n; y++){
+                boards[x][y] = now[y];
+                if(!now[y].equals(".") && !now[y].equals("*")){
+                    count += 1;
+                }
+            }
+        }
+        if(count % 2 != 0){
+            return "IMPOSSIBLE";
+        }
+        answers = new ArrayList<String>();
+        for(int x = 0; x < m; x++){
+            for(int y = 0; y < n; y++){
+                if(!boards[x][y].equals("*") && !boards[x][y].equals(".")){
+                    if(isPossible(x,y)){
+                        visited[x][y] = true;
+                        find(count-2, boards[x][y]);
+                    }
+                }
+            }
+        }
+        if(answers.size() == 0){
+            return "IMPOSSIBLE";
+        }else{
+            Collections.sort(answers);
+            return answers.get(0);
+        }
+    }
+    public static void main(String[] args){
+        int m = 3;
+        int n = 3;
+        String [] board = new String [] {"DBA", "C*A", "CDB"};
+        System.out.println(solution(m,n,board));
+    }
+}
---
+++ Java/Test/src/Weekely/NineWeek.java
@@ -0,0 +1,68 @@
+package Weekely;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class NineWeek {
+    static ArrayList<Integer>[] base;
+    public static int find(int n, int x, int y){
+        boolean [] visited;
+        ArrayList<Integer> al = new ArrayList<Integer>();
+        Queue<Integer> q;
+        for(int i = 1; i < n+1; i++){
+            if(base[i].size() != 0){
+                q = new LinkedList<Integer>();
+                q.add(i);
+                visited = new boolean[n+1];
+                visited[i] = true;
+                int count = 0;
+                while(!q.isEmpty()){
+                    int now = q.poll();
+                    count += 1;
+                    for(int toGo : base[now]){
+                        if(!visited[toGo]){
+                            if((now != x && toGo != y) && (now != y && toGo != x)){
+                                visited[toGo] = true;
+                                q.add(toGo);
+                            }
+                        }
+                    }
+                }
+                al.add(count);
+            }
+        }
+        if(al.size() == 2){
+            return Math.abs(al.get(0) - al.get(1));
+        }else{
+            return -1;
+        }
+    }
+    public static void init(int n, int [][] wires){
+        base = new ArrayList[n+1];
+        for(int i = 1; i < n+1; i++){
+            base[i] = new ArrayList<Integer>();
+        }
+        for(int i = 0 ; i < wires.length; i++){
+            int [] now = wires[i];
+            base[now[0]].add(now[1]);
+            base[now[1]].add(now[0]);
+        }
+    }
+    public static int solution(int n, int [][] wires){
+        init(n, wires);
+        int answer = Integer.MAX_VALUE;
+        for(int i = 0 ; i < wires.length; i++){
+            int temps = find(n, wires[i][0],wires[i][1]);
+            if(temps != -1){
+                answer = Math.min(temps, answer);
+            }
+        }
+        return answer;
+    }
+    public static void main(String[] args){
+        int n = 9;
+        int [][] wires = new int [][] {{1,3},{2,3},{3,4},{4,5},{4,6},{4,7},{7,8},{7,9}};
+        System.out.println(solution(n, wires));
+    }
+}
